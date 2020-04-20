f0699241ca5ddb01bf326077c61cfc4b005c97bc
unknown <yourGitHub@example.com>
2020-04-20 22:16:13 +0500

    now I have built the fields of addition, subtraction, multiplication and division in the delegate

--- Program.cs
+++ Program.cs
@@ -4,10 +4,31 @@ namespace HW_14
 {
     class Program
     {
+        delegate cases infomation<cases>(cases x, cases y);
       static void Main(string[] args)
       {
-          
+         
       }
+        
+      public static expencive sum<expencive>(expencive x, expencive y)
+      {
+          return (dynamic)x + (dynamic)y;
+      }
+      
+      public static expencive minus<expencive>(expencive x, expencive y)
+      {
+          return (dynamic)x-(dynamic)y;
+      }
+      
+      public static expencive multplay<expencive>(expencive x, expencive y)
+      {
+          return (dynamic)x*(dynamic)y;
+      }
+
+    public static expencive distribution<expencive>(expencive x, expencive y)
+    {
+        return (dynamic)x/(dynamic)y;
+    }
     }
 
 }
