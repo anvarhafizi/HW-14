0f4bfc408647b854d79779f2c6d7e5d3eccd4557
unknown <yourGitHub@example.com>
2020-04-20 22:24:20 +0500

    The Finnaly code my Programm

--- Program.cs
+++ Program.cs
@@ -7,7 +7,31 @@ namespace HW_14
         delegate cases infomation<cases>(cases x, cases y);
       static void Main(string[] args)
       {
-         
+          System.Console.Write("Enter the first number=");
+          double a=Convert.ToDouble(Console.ReadLine());
+          System.Console.Write("Enter the second number=");
+          double b=Convert.ToDouble(Console.ReadLine());
+
+          System.Console.WriteLine(@"What do you want to do? If you want 
+          +;
+          -;
+          *;
+          /;
+          ");
+      
+        infomation<double>info;
+        string operations=Console.ReadLine();
+        switch(operations)
+        {
+            case "+": info=sum;
+            System.Console.Write($"Answer {a}+{b}={info.Invoke(a,b)}"); break;
+            case "-": info=minus;
+            System.Console.Write($"Answer {a}-{b}={info.Invoke(a,b)}"); break;
+            case "*": info=multplay;
+            System.Console.Write($"Answer {a}*{b}={info.Invoke(a,b)}"); break;
+            case "/": info=distribution;
+            System.Console.WriteLine($"Answer {a}/{b}={info.Invoke(a,b)}"); break;
+        }
       }
         
       public static expencive sum<expencive>(expencive x, expencive y)
